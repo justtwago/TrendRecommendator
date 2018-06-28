4635deb716423f4c87b0072fdef7799165f6164c
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2018-06-28 23:10:28 +0200

    Colors

--- app/src/main/java/com/example/artyomvlasov/trendrecommendator/app/photo/PhotoResultActivity.kt
+++ app/src/main/java/com/example/artyomvlasov/trendrecommendator/app/photo/PhotoResultActivity.kt
@@ -3,6 +3,7 @@ package com.example.artyomvlasov.trendrecommendator.app.photo
 import android.annotation.TargetApi
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
@@ -16,6 +17,8 @@ import com.example.artyomvlasov.trendrecommendator.app.utils.Constants
 import com.example.artyomvlasov.trendrecommendator.app.utils.Constants.TYPE_KEY
 import com.example.artyomvlasov.trendrecommendator.app.utils.Constants.COLOR_KEY
 import com.example.artyomvlasov.trendrecommendator.app.utils.Constants.GENDER_KEY
+import com.example.artyomvlasov.trendrecommendator.tensorflow.DIM_IMG_SIZE_X
+import com.example.artyomvlasov.trendrecommendator.tensorflow.DIM_IMG_SIZE_Y
 import com.example.artyomvlasov.trendrecommendator.tensorflow.ImageClassifier
 import com.example.artyomvlasov.trendrecommendator.util.ClothesTypeManager
 import com.example.artyomvlasov.trendrecommendator.util.choiceHelper.*
@@ -27,7 +30,9 @@ private const val RULES_PATH = "rules.txt"
 
 class PhotoResultActivity : AppCompatActivity() {
     private val extras by lazy { intent.extras }
-    private val imageBitmap by lazy { extras!!.get("data") as Bitmap }
+    private val imageBitmap by lazy {
+        Bitmap.createScaledBitmap(extras!!.get("data") as Bitmap, DIM_IMG_SIZE_X, DIM_IMG_SIZE_Y, false)
+    }
     private val classifier by lazy { ImageClassifier(this) }
     private var clothesColor = ApiColor.Black
     private var clothesType = ApiType.UNKNOWN
@@ -45,7 +50,11 @@ class PhotoResultActivity : AppCompatActivity() {
 
     private fun setButtonsVisibility() {
         when (ApiCategory.categoriesByType[clothesType]) {
-            ApiCategory.TOP -> shirtIcon.visibility = View.GONE
+            ApiCategory.TOP -> {
+                shirtIcon.visibility = View.GONE
+                trousersIcon.setIconEnabled(true, false)
+                clothesCategory = ApiCategory.BOTTOM
+            }
             ApiCategory.BOTTOM -> trousersIcon.visibility = View.GONE
             ApiCategory.SHOES -> shoesIcon.visibility = View.GONE
         }
@@ -53,12 +62,20 @@ class PhotoResultActivity : AppCompatActivity() {
 
     private fun setClickListeners() {
         goButton.setOnClickListener {
-            val suggestedItem: ClothItem = HelperFactory.fromFile(this, RULES_PATH).suggest(ClothItem(clothesColor, clothesType), clothesCategory)
-            val intent = Intent(this, ClothesActivity::class.java)
-            intent.putExtra(COLOR_KEY, suggestedItem.color().name)
-            intent.putExtra(TYPE_KEY, suggestedItem.type().name)
-            intent.putExtra(GENDER_KEY, gender)
-            startActivity(intent)
+            progressLayout.visibility = View.VISIBLE
+            goButton.isEnabled = false
+            Thread {
+                val suggestedItem: ClothItem = HelperFactory.fromFile(this, RULES_PATH).suggest(ClothItem(clothesColor, clothesType), clothesCategory)
+                val intent = Intent(this, ClothesActivity::class.java)
+                intent.putExtra(COLOR_KEY, suggestedItem.color().name)
+                intent.putExtra(TYPE_KEY, suggestedItem.type().name)
+                intent.putExtra(GENDER_KEY, gender)
+                runOnUiThread {
+                    goButton.isEnabled = true
+                    progressLayout.visibility = View.GONE
+                }
+                startActivity(intent)
+            }.start()
         }
 
         maleIcon.setOnClickListener {
@@ -103,8 +120,8 @@ class PhotoResultActivity : AppCompatActivity() {
 
     private fun printClothesColorAndType() {
         val text = resources.getString(R.string.recognized_wear_info,
-                clothesColor.toString().toLowerCase(),
-                clothesType.toString().toLowerCase())
+                clothesColor.toString().toUpperCase(),
+                clothesType.toString().toUpperCase())
         infoText.text = text
     }
 
